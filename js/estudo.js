00c358f4eb20aff2d2fe37d0ba426ab941eb4701
Marcos Siqueira <106640310+Msiqueira1@users.noreply.github.com>
2022-12-14 11:06:01 -0300

    Add files via upload

---
+++ js/estudo.js
@@ -0,0 +1,128 @@
+/*
+console.log("Olá João e Maria!");
+
+let cao = "Pluto";
+console.log("O nome do meu cachorro é: " + cao);
+
+cao = "Toto";
+console.log("O nome do meu cachorro é: " + cao);
+
+let gato = "Miau";
+
+console.log("O nome do meu cachorro é: " + cao + " e o nome do meu gato é: " + gato + '.');
+
+let valor1 = 15;
+let valor2 = 50;
+let calculo = valor1 + valor2;
+
+console.log("A soma do valor " + valor1 + " com o valor " + valor2 + " é: " + calculo);
+
+let carros = ["uno", "gol", "fusca", "palio"];
+console.log ("Modelos de carros " + carros);
+
+
+
+let a = Boolean (4 > 5);
+console.log (a);*/
+
+/*let var  = require ("readLine-Sync");
+
+let nome = readLineSync.question ("Digite seu Nome: ");
+console.log (nome);*/
+
+/*
+Operadores Aritméticos
+Operador	Descrição
++			Adição
+-			Subtração
+*			Multiplicação
+**			Exponenciação
+/			Divisão
+%			Módulo (Resto de uma divisão)
+++			Incremento
+--			Decremento
+
+Operadores de Atribuição
+Operador	Exemplo	Outra forma de escrever
+=			x = y	x = y
++=			x += y	x = x + y
+-=			x -= y	x = x - y
+*=			x *= y	x = x * y
+/=			x /= y	x = x / y
+%=			x %= y	x = x % y
+
+Operadores de Comparação
+Operador	Nome			Exemplo	Resultado
+==			Igual			a == b	Compara se o valor da variável a é igual ao valor da variável b
+===			Idêntico		a === b	Compara se o valor e o tipo da variável a são iguais ao valor e o tipo da variável b
+!=			Diferente		a != b	Retorna verdadeiro se o valor da variável a é diferente de b
+!==			Não Idêntico	a !== b	Compara se o valor e o tipo da variável a são diferentes do valor e o tipo da variável b
+>			Maior que		a > b	Verifica se a é maior que b
+<			Menor que		a < b	Verifica se a é menor que b
+>=			Maior ou igual 	a >= b	Verifica se a é maior ou igual a b
+<=			Menor ou igual	a <= b	Verifica se a é menor ou igual a b
+
+Operadores Lógicos
+Operador	Nome	Exemplo		Resultado
+&&			e		a && b		Verdadeiro se a e b são verdadeiros
+||			ou		a || b		Verdadeiro se a ou b são verdadeiros
+!			Negação	!a			Verdadeiro se a não é verdadeiro
+*/
+
+/*
+let nome = "Marcos";
+if (nome == "Marcos"){
+	console.log ("Esse e meu nome: " + nome);
+	} 
+	else {
+		console.log ("Esse não e meu nome");
+		console.log ("O meu nome e: " + nome);
+	}
+  let hora = 19;
+  if (hora < 10) {
+   console.log("Bom dia!");
+  } else if (hora < 18) {
+    console.log("Boa tarde!");
+  } else {
+    console.log("Boa noite!");
+}
+
+
+let opcoes = 4;
+
+switch (opcoes) {
+  case 1:
+    console.log("Você escolheu a primeira opção.");
+    break;
+ case 2:
+    console.log("Você escolheu a segunda opção.");
+    break;
+ case 3:
+    console.log("Você escolheu a terceira opção.");
+    break;
+ default:
+    console.log("Opção inválida.");
+};*/
+
+ /*
+  for (a = 0; a <= 10; a++) {
+  console.log(a);
+  }
+
+
+  function soma(a, b) {
+  resultado = a + b;
+   return resultado;
+}
+
+ console.log(soma(10,10));*/
+  
+    let hora = 0;
+
+  if (hora < 12) {
+    console.log("Bom dia!");
+  } else if (hora = 18) {
+    console.log("Boa tarde!");
+  } else {
+    console.log("Boa noite!");
+}
